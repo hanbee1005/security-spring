5a87fa0b133198e4fdac7ae1e328dcddda56bec8
hanbee <sonhanbi1002@gmail.com>
2021-06-30 00:12:49 +0900

    로그인한 사용자 정보 Session에 저장

--- src/main/java/com/example/securityspring/service/AuthService.java
+++ src/main/java/com/example/securityspring/service/AuthService.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +27,8 @@ public class AuthService {
     public String login(JwtRequestDto request) throws Exception {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
         return principal.getUsername();
