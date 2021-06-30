1cce8081f4be6ead984479c9ac399bd68018a56f
hanbee1005 <sonhanbi1002@gmail.com>
2021-06-30 15:12:44 +0900

    security 설정에서 jwt token 사용으로 session 사용 x, jwt token filter 추가

--- src/main/java/com/example/securityspring/config/SecurityConfig.java
+++ src/main/java/com/example/securityspring/config/SecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.securityspring.config;
 
+import com.example.securityspring.security.JwtAuthenticationFilter;
+import com.example.securityspring.security.JwtTokenProvider;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public BCryptPasswordEncoder encodePassword() {  // 회원가입 시 비밀번호 암호화에 사용할 Encoder 빈 등록
@@ -48,7 +56,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // '/admin'의 경우 ADMIN 권한이 있는 사용자만 접근이 가능
                 .antMatchers("/admin").hasRole("ADMIN")
                 // 그 외 모든 요청은 인증과정 필요
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                // 토큰 기반 인증이기 때문에 session 사용 x
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // JwtAuthenticationFilter 는 UsernamePasswordAuthenticationFilter 전에 넣음
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
